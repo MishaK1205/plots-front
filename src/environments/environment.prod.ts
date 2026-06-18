c7b873e025024fac7da29b087c723bc9b97ae130
MishaK1205 <mikheil.kvashali1204@gmail.com>
2026-06-19 00:38:58 +0400

    minor change

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  googleMapsApiKey: 'AIzaSyAFCocUFDN7TG98EAByQ9KsUJcU7Cx-fAQ',
+  serverUrl: 'https://plots-back-production.up.railway.app',
+  apiUrl: 'https://plots-back-production.up.railway.app',
+};
