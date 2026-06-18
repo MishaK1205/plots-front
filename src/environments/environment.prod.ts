b9a55d5f5e9a01102632c59e544f0396fd574be1
MishaK1205 <mikheil.kvashali1204@gmail.com>
2026-06-19 01:39:55 +0400

    minor change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
+const serverUrl = 'https://plots-back-production.up.railway.app';
+
 export const environment = {
   production: true,
   googleMapsApiKey: 'AIzaSyAFCocUFDN7TG98EAByQ9KsUJcU7Cx-fAQ',
-  serverUrl: 'https://plots-back-production.up.railway.app',
-  apiUrl: 'https://plots-back-production.up.railway.app',
+  serverUrl,
+  apiUrl: `${serverUrl}/api`,
 };
